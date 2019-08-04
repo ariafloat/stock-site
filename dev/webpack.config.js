7fa06ded44ab5f7da7c9443ba262bdc87338ff5c
Egg Saito <ariafloat@users.noreply.github.com>
2019-08-04 22:07:48 +0900

    Update package

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['env', { modules: false }],
+              '@babel/preset-env',
             ],
           },
         },
